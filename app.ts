75b6921767829fbbc201bfa35cebd864bf3b56d0
Kah919 <kahyap13@gmail.com>
2021-01-10 23:01:02 -0500

    Added unions

--- app.ts
+++ app.ts
@@ -8,3 +8,17 @@ const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
 }
 
 add(1, 2, true, "testing");
+
+
+type Combinable = number | string; // a way to do union types
+
+const combine = (input1: Combinable, input2: Combinable) => {
+    const result = input1 + input2;
+    return result;
+}
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
